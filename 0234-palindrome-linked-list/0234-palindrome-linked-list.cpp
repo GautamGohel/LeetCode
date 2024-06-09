a07cf9f43e113fac19badf2bc4dad375bfcad907
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-06-09 11:11:08 +0530

    Time: 261 ms (5.04%), Space: 119 MB (59.91%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,31 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head) {
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-        while (curr != nullptr) {
-            ListNode* next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
     
-    bool isPalindrome(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
+    ListNode* reversell(ListNode* head){
+        if(!head or !head->next){
+            return head;
         }
-        ListNode* rev = reverse(slow);
-        while (rev != nullptr) {
-            if (head->val != rev->val) {
+      ListNode* new_head=reversell(head->next);
+      head->next->next=head;
+      head->next=NULL;
+      return new_head;
+    } 
+
+    bool isPalindrome(ListNode* head) {
+        /* Brute force
+               stack<int>st;
+        ListNode* temp=head;
+        while(temp){
+            st.push(temp->val);
+             temp=temp->next;
+        }
+        temp=head;
+        while(temp){
+            if(temp->val!=st.top()){
                 return false;
             }
-            head = head->next;
-            rev = rev->next;
+            temp=temp->next;
+            st.pop();
+        }
+        return true;
+        */
+        if(!head or !head->next){
+            return true;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next and fast->next->next ){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* newHead=reversell(slow->next);
+        ListNode* temp1=head;
+        ListNode* temp2=newHead;
+        while(temp2){
+            if(temp1->val!=temp2->val){
+                return false;
+            }
+            temp1=temp1->next;
+            temp2=temp2->next;
         }
         return true;
     }
