13bc32a8554248246321e135cc2070041d385a34
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-07-09 10:52:17 +0530

    Time: 0 ms (100%), Space: 7.3 MB (11.83%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
+   /* Recursion
     int fib(int n) {
         if(n<=1)
         return n;
         return fib(n-1)+fib(n-2);
+    }*/
+    int f(int n,vector<int>&dp){
+        if(n<=1){
+            return n;
+        }
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=f(n-1,dp)+f(n-2,dp);
+    }
+    int fib(int n) {
+        vector<int>dp(n+1,-1);
+       return f(n,dp);
     }
 };
