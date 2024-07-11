68f9b609687f98bb82e4d9b3d438dcb48d351002
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-07-11 21:12:26 +0530

    Time: 0 ms (100%), Space: 8 MB (17.53%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -12,7 +12,7 @@ public:
        return solve(m-1,n-1);
     }
     */
-    int solve(int m,int n,vector<vector<int>>&dp){
+    /*int solve(int m,int n,vector<vector<int>>&dp){
        if(m==0 and n==0) return 1;
         if(m<0 or n<0) return 0;
         if(dp[m][n]!=-1) return dp[m][n];
@@ -23,5 +23,21 @@ public:
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
        return solve(m-1,n-1,dp);
+    }
+    */
+    int uniquePaths(int m, int n) {
+    vector<vector<int>>dp(m,vector<int>(n,-1));
+    dp[0][0]=1;
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 and j==0) continue;
+            int up=0;
+            int left=0;
+            if(i-1>=0) up=dp[i-1][j];
+            if(j-1>=0) left=dp[i][j-1];
+            dp[i][j]=up+left;
+        }
+       }
+       return dp[m-1][n-1];
     }
 };
