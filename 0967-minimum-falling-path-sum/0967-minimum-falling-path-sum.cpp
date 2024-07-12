a2b32b44f410f55414dbc2ebdfcb7bae6accb018
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-07-12 20:20:37 +0530

    Time: 12 ms (59.62%), Space: 13.9 MB (8.46%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,45 +1,24 @@
 class Solution {
 public:
-     /*
-    int solve(int i, int j, vector<vector<int>>& matrix, int n,vector<vector<int>>&dp) {
-        if (j < 0 || j >= n) return INT_MAX;  
-        if (i == 0) return matrix[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ld = solve(i - 1, j - 1, matrix, n,dp);
-        int up = solve(i - 1, j, matrix, n,dp);
-        int rd = solve(i - 1, j + 1, matrix, n,dp);
-        return dp[i][j]=matrix[i][j] + min(ld, min(up, rd));
-    }
-
-    int minFallingPathSum(vector<vector<int>>& matrix) {
+      int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        int mini = INT_MAX;
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        for (int i = 0; i < n; i++) {
-            mini = min(mini, solve(n - 1, i, matrix, n,dp));
-        }
-        return mini;
-    }
-    */
-
-
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
+        vector<int>prev(n,0);
         for(int i=0;i<n;i++){
-            dp[0][i]=matrix[0][i];
+            prev[i]=matrix[0][i];
         }
         for(int i=1;i<n;i++){
+            vector<int>curr(n,0);
             for(int j=0;j<n;j++){
-               int ld=j>0?dp[i-1][j-1]:INT_MAX;
-               int up=dp[i-1][j];
-               int rd=j<n-1?dp[i-1][j+1]:INT_MAX;
-               dp[i][j]=matrix[i][j]+min(ld,min(up,rd));
+               int ld=j>0?prev[j-1]:INT_MAX;
+               int up=prev[j];
+               int rd=j<n-1?prev[j+1]:INT_MAX;
+               curr[j]=matrix[i][j]+min(ld,min(up,rd));
             }
+            prev=curr;
         }
         int mini = INT_MAX;
         for (int i = 0; i < n; i++) {
-            mini = min(mini, dp[n-1][i]);
+            mini = min(mini, prev[i]);
         }
         return mini;
     }
