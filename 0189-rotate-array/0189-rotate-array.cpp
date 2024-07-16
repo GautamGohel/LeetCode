72e16dd58b4ac8988acc02bd1ba7b29bc5677603
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-07-16 19:30:27 +0530

    Time: 21 ms (55.4%), Space: 27.6 MB (40.62%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+
+       //brute force tc O(n) sc O(n)
+        // int n = nums.size();
+        // k=k%n;
+        // vector<int> temp(n);
+        // int j =n-k;
+        // for (int i = 0; i < n; i++) {
+        //    temp[i] = nums[j % n];
+        //     j++;
+        //  }
+        //  nums = temp;
+         
+         //optimized space tc O(n)
           int n=nums.size();
           k=k%n;
           reverse(nums.begin(),nums.end());
