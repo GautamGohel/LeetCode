c4d355d1ff17a79f781094df30beeef1378b606a
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-07-17 21:14:56 +0530

    Time: 4 ms (38.31%), Space: 8.9 MB (8.79%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+       //brute force
+        //  int n=matrix.size();
+        //  vector<vector<int>>rotated(n,vector<int>(n,0));
+        //  for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         rotated[j][n-i-1]=matrix[i][j];
+        //     }
+        //  }
+        // matrix=rotated;
+
         int n=matrix.size();
         for(int i=0;i<n-1;i++){
            for(int j=i+1;j<n;j++){
