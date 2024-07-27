cfedb9169112b2b9b5f44b541c9e9af1968d0fb8
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-07-27 13:16:24 +0530

    Time: 11 ms (86.19%), Space: 31.6 MB (26.37%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    // int solve(string s,string t,int i,int j,vector<vector<int>>dp){
+    //     if(j<0) return 1;
+    //     if(i<0) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(s[i]==t[j]){
+    //         return solve(s,t,i-1,j-1,dp)+solve(s,t,i-1,j,dp);
+    //     }
+    //     return dp[i][j]=solve(s,t,i-1,j,dp);
+    // }
+   
+    // int numDistinct(string s, string t) {
+    //     int n1=s.length();
+    //     int n2=t.length();
+    //     vector<vector<int>>dp(n1,vector<int>(n2,-1));
+    //     return solve(s,t,n1-1,n2-1,dp);
+    // }
+
+    int numDistinct(string s, string t) {
+        int n1=s.length();
+        int n2=t.length();
+        vector<vector<double>>dp(n1+1,vector<double>(n2+1,0));
+        for(int i=0;i<=n1;i++){
+            dp[i][0]=1;
+        }
+        // for(int j=1;j<=n2;j++){
+        //     dp[0][j]=0;
+        // } don't need as all are allready intitiallized with 0
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                 if(s[i-1]==t[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else dp[i][j]=dp[i-1][j];
+            }
+        }
+        return (int)dp[n1][n2];
+    }
+};
