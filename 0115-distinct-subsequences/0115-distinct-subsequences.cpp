0d6d560bceeffd52f18cab86ae928fe24749ff36
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-07-27 13:22:30 +0530

    Time: 14 ms (77.93%), Space: 31.3 MB (38.16%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -18,24 +18,45 @@ public:
     //     return solve(s,t,n1-1,n2-1,dp);
     // }
 
+    // int numDistinct(string s, string t) {
+    //     int n1=s.length();
+    //     int n2=t.length();
+    //     vector<vector<double>>dp(n1+1,vector<double>(n2+1,0));
+    //     for(int i=0;i<=n1;i++){
+    //         dp[i][0]=1;
+    //     }
+    //     // for(int j=1;j<=n2;j++){
+    //     //     dp[0][j]=0;
+    //     // } don't need as all are allready intitiallized with 0
+    //     for(int i=1;i<=n1;i++){
+    //         for(int j=1;j<=n2;j++){
+    //              if(s[i-1]==t[j-1]){
+    //                 dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+    //             }
+    //             else dp[i][j]=dp[i-1][j];
+    //         }
+    //     }
+    //     return (int)dp[n1][n2];
+    // }
     int numDistinct(string s, string t) {
         int n1=s.length();
         int n2=t.length();
-        vector<vector<double>>dp(n1+1,vector<double>(n2+1,0));
-        for(int i=0;i<=n1;i++){
-            dp[i][0]=1;
-        }
+        vector<double>prev(n2+1,0);
+            prev[0]=1;
         // for(int j=1;j<=n2;j++){
         //     dp[0][j]=0;
         // } don't need as all are allready intitiallized with 0
         for(int i=1;i<=n1;i++){
+            vector<double>curr(n2+1,0);
+            curr[0]=1;
             for(int j=1;j<=n2;j++){
                  if(s[i-1]==t[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                    curr[j]=prev[j-1]+prev[j];
                 }
-                else dp[i][j]=dp[i-1][j];
+                else curr[j]=prev[j];
             }
+            prev=curr;
         }
-        return (int)dp[n1][n2];
+        return (int)prev[n2];
     }
 };
