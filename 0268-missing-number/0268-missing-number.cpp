b58b2d87107da1b7cf4ad39d7f9bc56e6bc66f25
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-08-15 16:51:07 +0530

    Time: 7 ms (96.47%), Space: 20.7 MB (32.74%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -14,17 +14,39 @@ public:
     //     return n;
     // }
 
+    // int missingNumber(vector<int>& nums) {
+    //    int n=nums.size();
+    //    vector<int>hash(n+1,0);
+    //    for(int i=0;i<n;i++){
+    //        hash[nums[i]]++;
+    //    }
+    //    for(int i=0;i<=n;i++){
+    //       if(hash[i]==0){
+    //         return i;
+    //       }
+    //    }
+    //    return n;
+    // }
+
+    //  int missingNumber(vector<int>& nums) {
+    //    int n=nums.size();
+    //    int sum=n*(n+1)/2;
+    //    int s=0;
+    //    for(int i=0;i<n;i++){
+    //      s+=nums[i];
+    //    }
+    //    return sum-s;
+    //  }
+
     int missingNumber(vector<int>& nums) {
        int n=nums.size();
-       vector<int>hash(n+1,0);
-       for(int i=0;i<n;i++){
-           hash[nums[i]]++;
-       }
+       int x=0;
        for(int i=0;i<=n;i++){
-          if(hash[i]==0){
-            return i;
-          }
+           x=x^i;
        }
-       return n;
-    }
+       for(int i=0;i<n;i++){
+          x=x^nums[i];
+       }
+       return x;
+     }
 };
