e49fa1f342706dc3f8cbbbbb20a0d39ca78f581e
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-08-25 10:31:36 +0530

    Time: 27 ms (83.77%), Space: 21.7 MB (64.55%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-int minEatingSpeed(vector<int>& piles, int h) {
-int left = 1, right = *max_element(piles.begin(), piles.end());
-while (left < right) {
-int mid = left + (right - left) / 2;
-int hours = 0;
-for (int p : piles) {
-hours += (p + mid - 1) / mid;
-}
-if (hours <= h) {
-right = mid;
-} else {
-left = mid + 1;
-}
-}
-return left;
-}
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1;
+        int high = *max_element(piles.begin(), piles.end());
+        int ans = high;  // Initialize ans with the maximum possible value
+
+        while (l <= high) {
+            int mid = l + (high - l) / 2;
+            if (canEatAll(piles, h, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+private:
+    bool canEatAll(const vector<int>& piles, int h, int k) {
+        long long hours = 0;  // Use long long to store the number of hours
+
+        for (int pile : piles) {
+            hours += (pile + k - 1) / k;  // This is equivalent to ceil(pile / k)
+            if (hours > h) {
+                return false;
+            }
+        }
+        return true;
+    }
 };
