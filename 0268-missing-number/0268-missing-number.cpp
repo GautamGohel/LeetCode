bec57ad8115f203e3caaeeebae1936b18bb6a238
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-10-29 19:50:27 +0530

    Time: 0 ms (100%), Space: 20.7 MB (75.35%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,52 +1,12 @@
 class Solution {
 public:
-    // int missingNumber(vector<int>& nums) {
-    //     int n=nums.size();
-    //     for(int i=0;i<n;i++){
-    //         bool flag=false;
-    //         for(int j=0;j<n;j++){
-    //             if(nums[j]==i){
-    //                 flag=true;
-    //             }
-    //         }
-    //         if(flag==false) return i;
-    //     }
-    //     return n;
-    // }
-
-    // int missingNumber(vector<int>& nums) {
-    //    int n=nums.size();
-    //    vector<int>hash(n+1,0);
-    //    for(int i=0;i<n;i++){
-    //        hash[nums[i]]++;
-    //    }
-    //    for(int i=0;i<=n;i++){
-    //       if(hash[i]==0){
-    //         return i;
-    //       }
-    //    }
-    //    return n;
-    // }
-
-    //  int missingNumber(vector<int>& nums) {
-    //    int n=nums.size();
-    //    int sum=n*(n+1)/2;
-    //    int s=0;
-    //    for(int i=0;i<n;i++){
-    //      s+=nums[i];
-    //    }
-    //    return sum-s;
-    //  }
-
     int missingNumber(vector<int>& nums) {
-       int n=nums.size();
-       int x=0;
-       for(int i=0;i<=n;i++){
-           x=x^i;
-       }
-       for(int i=0;i<n;i++){
-          x=x^nums[i];
-       }
-       return x;
-     }
+        int sum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        int ans=(n*(n+1))/2-sum;
+        return ans;
+    }
 };
