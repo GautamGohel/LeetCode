58ed5733271d098de5e6e0b2d625087beddf75ab
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-11-02 20:04:37 +0530

    Time: 0 ms (100%), Space: 19.6 MB (65.42%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,53 +1,11 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& arr) {
-
-    //brute force O(n2)     
-    //      int n = arr.size();
-    // for (int i = 0; i < n; i++) {
-    //     int num = arr[i];
-    //     int cnt = 0;
-    //     for (int j = 0; j < n; j++) {
-    //         if (arr[j] == num)
-    //             cnt++;
-    //     }
-    //     if (cnt == 1) return num;
-    // }
-    // return -1;
-    //hashing O(3n) O(max(arr))
-    // int n=arr.size();
-    // int maxi=0;
-    // for(int i=0;i<n;i++){
-    //     maxi=max(maxi,arr[i]);
-    // }
-    // vector<int>hash(maxi+1,0);
-    // for(int i=0;i<n;i++){
-    //     hash[arr[i]]++;
-    // }
-    // for(int i=0;i<n;i++){
-    //     if(hash[arr[i]]==1)
-    //     return arr[i];
-    // }
-    // return -1;
-
-    //using map  tc O(nlogn +m) sc O(m)
-    // int n = arr.size();
-    // map<int, int> mpp;
-    // for (int i = 0; i < n; i++) {
-    //     mpp[arr[i]]++;
-    // }
-
-    // for (auto it : mpp) {
-    //     if (it.second == 1)
-    //         return it.first;
-    // }
-    // return -1;
-
-    //using bitwise operator    
-    int XOR=0;
-    for(int i=0;i<arr.size();i++){
-        XOR=XOR^arr[i];
-    }
-    return XOR;
+    int singleNumber(vector<int>& nums) {
+        int xorr=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            xorr=xorr^nums[i];
+        }
+        return xorr;
     }
 };
