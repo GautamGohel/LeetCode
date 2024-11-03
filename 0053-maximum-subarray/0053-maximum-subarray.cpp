4154cee628ec1467002b492ca59c91725a7244c3
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-11-03 17:30:23 +0530

    Time: 0 ms (100%), Space: 70.7 MB (21.73%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // brute force O(n2)
-        // int n=nums.size();
-        // int maxi=INT_MIN;
-        // for(int i=0;i<n;i++){
-        //     int sum=0;
-        //    for(int j=i;j<n;j++){
-        //       sum+=nums[j];
-        //       maxi=max(maxi,sum);
-        //    }
-        // }
-        // return maxi;
-
-        //kadane's algorithm
-
-        int n=nums.size();
         int sum=0;
-        int maxi=INT_MIN;
+        int maxsum=INT_MIN;
+        int n=nums.size();
         for(int i=0;i<n;i++){
             sum+=nums[i];
-            if(sum>maxi)
-            maxi=sum;
+            maxsum=max(maxsum,sum);
             if(sum<=0){
                 sum=0;
             }
         }
-        return maxi;
+        return maxsum;
     }
 };
