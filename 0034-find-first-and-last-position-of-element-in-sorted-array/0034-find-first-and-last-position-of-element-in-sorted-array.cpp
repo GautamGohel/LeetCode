604b07e96e75995146ce6c26eadfe411af44498c
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-11-07 13:05:41 +0530

    Time: 0 ms (100%), Space: 16.4 MB (39.73%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,43 @@
 class Solution {
 public:
-
-   int getfirstele(vector<int>nums,int target){
-     int n=nums.size();
-     int low=0,high=n-1;
-     int res=-1;
-     while(low<=high){
-        int mid=(low+high)/2;
-        if(nums[mid]==target){
-            res=mid;
-            high=mid-1;
-        }
-        else if(nums[mid]<target){
-            low=mid+1;
-        }
-        else{
-            high=mid-1;
-        }
-     }
-     return res;
-   }
-
-    int getlastele(vector<int>nums,int target){
+ 
+    int firstocc(vector<int>&nums,int target){
         int n=nums.size();
+        int l=0,h=n-1;
         int res=-1;
-        int low=0,high=n-1;
-        while(low<=high){
-            int mid=(low+high)/2;
+        while(l<=h){
+            int mid=(l+h)/2;
             if(nums[mid]==target){
-                res=mid;
-                low=mid+1;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
+               res=mid;
+               h=mid-1;
+            }else if(nums[mid]<target){
+               l=mid+1;
+            }else{
+                h=mid-1;
             }
         }
         return res;
-   }
+    }
 
+    int lastocc(vector<int>&nums,int target){
+        int n=nums.size();
+        int l=0,h=n-1;
+        int res=-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(nums[mid]==target){
+               res=mid;
+               l=mid+1;
+            }else if(nums[mid]>target){
+               h=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return res;
+    }
+  
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int>ans;
-        ans.push_back(getfirstele(nums,target));
-        ans.push_back(getlastele(nums,target));
-        return ans;
+        return {firstocc(nums,target),lastocc(nums,target)};
     }
 };
