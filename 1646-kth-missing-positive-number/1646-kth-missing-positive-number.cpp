061f96c19714a69065abeaeb34f05e0c834a1884
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-11-13 18:18:40 +0530

    Time: 0 ms (100%), Space: 12.2 MB (96%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -4,7 +4,7 @@ public:
         int n=arr.size();
         int l=0,h=n-1;
         while(l<=h){
-            int mid=(l+h)/2;
+            int mid=(h+l)/2;
             if(arr[mid]-mid-1<k){
                 l=mid+1;
             }else{
