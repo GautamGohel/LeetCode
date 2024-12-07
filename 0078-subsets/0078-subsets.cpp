387f42b33b4ef75dbc5c4a539979ec452cdf477b
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-12-07 18:40:22 +0530

    Time: 0 ms (100%), Space: 15.3 MB (11.92%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-
-    void printallsubset(int i,vector<int>nums,vector<vector<int>>&ds,vector<int>ans,int n){
-        if(i==n){
-            ds.push_back(ans);
-            return ;
+    
+    void getallsubsets(int i,vector<int>arr,int n,vector<int>&ans,vector<vector<int>>&db){
+        if(i>=n){
+            db.push_back(ans);
+            return;
         }
-        ans.push_back(nums[i]);
-        printallsubset(i+1,nums,ds,ans,n);
+        ans.push_back(arr[i]);
+        getallsubsets(i+1,arr,n,ans,db);
         ans.pop_back();
-        printallsubset(i+1,nums,ds,ans,n);
+        getallsubsets(i+1,arr,n,ans,db);
     }
-   
+  
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>ans;
         vector<vector<int>>ds;
-        printallsubset(0,nums,ds,ans,n);
+        vector<int>ans;
+        int n=nums.size();
+        getallsubsets(0,nums,n,ans,ds);
         return ds;
     }
 };
