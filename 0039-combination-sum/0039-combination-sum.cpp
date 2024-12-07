f4cf11601637af9accdc3f530af554d29bdde9c9
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2024-12-07 20:32:08 +0530

    Time: 3 ms (56.25%), Space: 14.1 MB (42.16%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-   
-   void findcombination(int i,vector<int>&a,int target,vector<vector<int>>&finalres,vector<int> ds){
-     if(i==a.size()){
-        if(target==0){
-            finalres.push_back(ds);
-        }
+    void getallsubsets(int i, vector<int>& arr, int n, int sum, vector<int>& ans, vector<vector<int>>& ds, int target) {
+        if (sum == target) {
+            ds.push_back(ans); 
             return;
-     }
-     if(a[i]<=target){
-     ds.push_back(a[i]);
-     findcombination(i,a,target-a[i],finalres,ds);
-     ds.pop_back();
-     }
-     findcombination(i+1,a,target,finalres,ds);
-   }
+        }
+        if (i == n || sum > target) {
+            return; 
+        }
+        ans.push_back(arr[i]);
+        getallsubsets(i, arr, n, sum + arr[i], ans, ds, target);
+        ans.pop_back(); 
+        getallsubsets(i + 1, arr, n, sum, ans, ds, target);
+    }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>finalres;
-        vector<int>ds;
-        findcombination(0,candidates,target,finalres,ds);
-        return finalres;
+        vector<vector<int>> ds; 
+        vector<int> ans;        
+        int n = candidates.size();
+
+        getallsubsets(0, candidates, n, 0, ans, ds, target);
+        return ds;
     }
 };
