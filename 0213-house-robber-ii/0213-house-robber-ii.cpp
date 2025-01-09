8bd2185d0e31923fa83416e29a6bcc7ce43156e6
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2025-01-09 18:58:42 +0530

    Time: 3 ms (7.31%), Space: 11.1 MB (7.98%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
-    int fn(vector<int>&nums) {
-    int n=nums.size();
-    int prev2=0;
-    int prev=nums[0];
-        int ans;
+
+    int solve(vector<int>nums){
+        int n=nums.size();
+        int prev2=0;
+        int prev=nums[0];
         for(int i=1;i<n;i++){
-           int pick=nums[i];
-           if(i>1) pick+=prev2;
-           int notpick=prev;
-           ans=max(pick,notpick);
-           prev2=prev;
-           prev=ans;
+            int pick=nums[i];
+            if(i>1) pick+=prev2;
+            int notpick=prev;
+            int curr=max(pick,notpick);
+            prev2=prev;
+            prev=curr;
         }
         return prev;
     }
-    int rob(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
-        vector<int>temp1,temp2;
-        for(int i=0;i<nums.size();i++){
-            if(i!=0) temp1.push_back(nums[i]);
-            if(i!=nums.size()-1) temp2.push_back(nums[i]);
+
+    int robhouse(vector<int>nums,int n){
+        if(n==1){
+            return nums[0];
         }
-        return max(fn(temp1),fn(temp2));
+        vector<int>arr1;
+        vector<int>arr2;
+        for(int i=0;i<n;i++){
+            if(i!=0) arr1.push_back(nums[i]);
+            if(i!=n-1) arr2.push_back(nums[i]);
+        }
+        int ans1=solve(arr1);
+        int ans2=solve(arr2);
+        return max(ans1,ans2);
+    }
+
+    int rob(vector<int>& nums) {
+       int n=nums.size();
+       return robhouse(nums,n); 
     }
 };
