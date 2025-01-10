326d12deb91abee3a35bea15a40bfc5ac0f46a60
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2025-01-10 12:05:00 +0530

    Time: 0 ms (100%), Space: 12.8 MB (53.21%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,17 +3,19 @@ public:
 
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
+        vector<int>prev(n,0);
         for(int i=0;i<n;i++){
-            dp[n-1][i]=triangle[n-1][i];
+            prev[i]=triangle[n-1][i];
         }
         for(int i=n-2;i>=0;i--){
+            vector<int>temp(n,0);
             for(int j=i;j>=0;j--){
-                int down=triangle[i][j]+dp[i+1][j];
-                int cross=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(down,cross);
+                int down=triangle[i][j]+prev[j];
+                int cross=triangle[i][j]+prev[j+1];
+                temp[j]=min(down,cross);
             }
+            prev=temp;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
