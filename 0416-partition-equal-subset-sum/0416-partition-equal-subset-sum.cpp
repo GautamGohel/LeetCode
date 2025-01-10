6b2d764ada3714dbd9e44375455e0e209671b0bc
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2025-01-10 19:10:53 +0530

    Time: 145 ms (40.94%), Space: 16.9 MB (67.06%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    bool subsetSumToK(int n, int k, vector<int> &arr) {
-        vector<bool> prev(k+1, false);
-        prev[0] = true;
-        if (arr[0] <= k) {
-            prev[arr[0]] = true;
+   
+    bool canPartition(vector<int>& arr) {
+        int sum=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
         }
-        for (int i = 1; i < n; i++) {
-            vector<bool> curr(k+1, false);
-            curr[0] = true;
-            for (int target = 1; target <= k; target++) {
-                bool notTaken = prev[target];
-                bool taken = false;
-                if (target >= arr[i]) {
-                    taken = prev[target - arr[i]];
+        if(sum%2!=0) return false;
+        int target=sum/2;
+        vector<vector<bool>>dp(n,vector<bool>(target+1,false));
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
+        }
+        if(arr[0]<=target){
+            dp[0][arr[0]]=true;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=target;j++){
+                bool pick=false;
+                if(arr[i]<=j){
+                    pick=dp[i-1][j-arr[i]];
                 }
-                curr[target] = taken || notTaken;
+                bool notpick=dp[i-1][j];
+                dp[i][j]=pick or notpick;
             }
-            prev = curr;
         }
-        return prev[k];
-    }
-
-    bool canPartition(vector<int>& nums) {
-        int total = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            total += nums[i];
-        }
-        if (total % 2 != 0) return false;
-        int target = total / 2;
-        return subsetSumToK(nums.size(), target, nums);
+        return dp[n-1][target];
     }
 };
