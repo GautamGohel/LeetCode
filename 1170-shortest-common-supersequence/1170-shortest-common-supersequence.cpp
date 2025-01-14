7476700fba898c9b748e617aba7a944d7a8c2ab1
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2025-01-14 21:23:43 +0530

    Time: 23 ms (87.18%), Space: 26.9 MB (38.7%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,49 +1,45 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string text1, string text2) {
-            int n1=text1.size();
-            int n2=text2.size();
-            vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
-            //as base case is if n1 or n2 is negative return 0 we can't use negative index
-            //so we're shifting indexes by 1 place 
-            for(int i=0;i<=n1;i++){
-                dp[i][0]=0;
-            }
-            for(int j=0;j<=n2;j++){
-                dp[0][j]=0;
-            }
-            for(int i=1;i<=n1;i++){
-                for(int j=1;j<=n2;j++){
-                    if(text1[i-1]==text2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                    else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-            int i=n1,j=n2;
-            string s="";
-            while(i>0 and j>0){
-                if(text1[i-1]==text2[j-1]){
-                   s+=text1[i-1];
-                   i--;
-                   j--;
-                }
-                else if(dp[i-1][j]==dp[i][j]){
-                   s+=text1[i-1];
-                   i--;
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n1=str1.size();
+        int n2=str2.size();
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(str1[i-1]==str2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
-                    s+=text2[j-1];
-                    j--;
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
-            while(i>0){
-                s+=text1[i-1];
+        }
+        int len=dp[n1][n2];
+        int i=n1,j=n2;
+        string s;
+        while(i>0 and j>0){
+            if(str1[i-1]==str2[j-1]){
+                s+=str1[i-1];
                 i--;
-            }
-            while(j>0){
-                s+=text2[j-1];
                 j--;
             }
-            reverse(s.begin(),s.end());
-         return s;   
+            else if(dp[i-1][j]>dp[i][j-1]){
+                s+=str1[i-1];
+                i--;
+            }else{
+                s+=str2[j-1];
+                j--;
+            }
+        }
+        while(i>0){
+            s+=str1[i-1];
+            i--;
+        }
+        while(j>0){
+            s+=str2[j-1];
+            j--;
+        }
+        reverse(s.begin(),s.end());
+        return s;
     }
 };
