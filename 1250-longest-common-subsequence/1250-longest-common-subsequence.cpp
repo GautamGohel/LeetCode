7faf79e920314295b8c98c5ccf219ed49808fb18
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2025-01-14 11:24:09 +0530

    Time: 23 ms (78.78%), Space: 27.5 MB (23.25%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -20,11 +20,11 @@ public:
         int n2=text2.size();
         vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
         dp[0][0]=0;
-        for(int i=0;i<n1;i++){
-            for(int j=0;j<n2;j++){
-                if(text1[i]==text2[j]) dp[i+1][j+1]=1+dp[i][j];
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(text1[i-1]==text2[j-1]) dp[i][j]=1+dp[i-1][j-1];
                 else{
-                    dp[i+1][j+1]=max(dp[i][j+1],dp[i+1][j]);
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
