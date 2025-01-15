a8d43504e7d9f1c6269cfdd00f812f1bf9329173
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2025-01-15 13:53:18 +0530

    Time: 7 ms (62.18%), Space: 19 MB (16.9%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,89 +1,27 @@
 class Solution {
 public:
-
-    // int solve(vector<int>&prices,int i,int n,int buy,vector<vector<vector<int>>>&dp,int cap){
-    //     if(i==n or cap==0){
-    //         return 0;
-    //     }
-    //     if(dp[i][buy][cap]!=-1) return dp[i][buy][cap];
-    //     int profit=0;
-    //     if(buy){
-    //         int profit=max(-prices[i]+solve(prices,i+1,n,0,dp,cap),solve(prices,i+1,n,1,dp,cap));
-    //         return dp[i][buy][cap]=profit;
-    //     }
-    //     else{
-    //         int profit=max(prices[i]+solve(prices,i+1,n,1,dp,cap-1),solve(prices,i+1,n,0,dp,cap));
-    //         return dp[i][buy][cap]=profit;
-    //     }
-    // }
-     
-    // int maxProfit(int k, vector<int>& prices) {
-    //     int n=prices.size();
-    //     vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
-    //     return solve(prices,0,n,1,dp,k);
-    // }
-
-    //  int maxProfit(int k,vector<int>& prices) {
-    //     int n=prices.size();
-    //     vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
-    //     //don't need base case as default is already 0
-    //     // for(int i=0;i<n;i++){
-    //     //     for(int buy=0;buy<=1;buy++){
-    //     //         dp[i][buy][0]=0;
-    //     //     }
-    //     // }
-    //     // for(int buy=0;buy<=1;buy++){
-    //     //    for(int cap=0;cap<=2;cap++){
-    //     //      dp[n][buy][cap]=0;
-    //     //    }
-    //     // }
-    //     for(int ind=n-1;ind>=0;ind--){
-    //         for(int buy=0;buy<=1;buy++){
-    //            for(int cap=1;cap<=k;cap++){
-    //              int profit=0;
-    //                 if(buy){
-    //                     int profit=max(-prices[ind]+dp[ind+1][0][cap],dp[ind+1][1][cap]);
-    //                     dp[ind][buy][cap]=profit;
-    //                 }
-    //                 else{
-    //                     int profit=max(prices[ind]+dp[ind+1][1][cap-1],dp[ind+1][0][cap]);
-    //                     dp[ind][buy][cap]=profit;
-    //                 }
-    //            }
-    //         }
-    //     }
-    //     return dp[0][1][k];
-    // }
-     int maxProfit(int k,vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>>after(2,vector<int>(k+1,0));
-        //default is already 0 so we don't have to write 0 in base case
-        //     for(int buy=0;buy<=1;buy++){
-        //         prev[buy][0]=0;
-        //     }
-      
-        // for(int buy=0;buy<=1;buy++){
-        //    for(int cap=0;cap<=2;cap++){
-        //      dp[n][buy][cap]=0;
-        //    }
-        // }
-        for(int ind=n-1;ind>=0;ind--){
-            vector<vector<int>>curr(2,vector<int>(k+1,0));
-            for(int buy=0;buy<=1;buy++){
-               for(int cap=1;cap<=k;cap++){
-                 int profit=0;
-                    if(buy){
-                        int profit=max(-prices[ind]+after[0][cap],after[1][cap]);
-                        curr[buy][cap]=profit;
+    int maxProfit(int cap, vector<int>& prices) {
+         int n=prices.size();
+        vector<vector<int>>prev(2,vector<int>(cap+1,0));
+        int profit;
+        for(int i=n-1;i>=0;i--){
+        vector<vector<int>>curr(2,vector<int>(cap+1,0));
+            for(int j=0;j<=1;j++){
+                for(int k=1;k<=cap;k++){
+                    if(j==0){
+                        int buyit=-prices[i]+prev[!j][k];
+                        int notbuy=prev[j][k];
+                        profit=max(buyit,notbuy);
+                    }else{
+                        int sellit=prices[i]+prev[!j][k-1];
+                        int notsell=prev[j][k];
+                        profit=max(sellit,notsell);
                     }
-                    else{
-                        int profit=max(prices[ind]+after[1][cap-1],after[0][cap]);
-                        curr[buy][cap]=profit;
-                    }
-               }
+                    curr[j][k]=profit;
+                }
             }
-            after=curr;
+            prev=curr;
         }
-        return after[1][k];
-    }//we can already do this using just 4 sized array check tuf
+        return prev[0][cap];
+    }
 };
