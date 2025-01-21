eb074c342c9b02cbea359e06b225c9758dc2a567
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2025-01-21 21:52:08 +0530

    Time: 3 ms (59.28%), Space: 66.6 MB (76.92%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
-        long long min_result = LLONG_MAX;  // Start with a large number for the minimum score.
-        long long row1_sum = accumulate(grid[0].begin(), grid[0].end(), 0LL);  // Top row sum
-        long long row2_sum = 0;  // Bottom row sum starts at 0.
-
-        for (int i = 0; i < grid[0].size(); ++i) {
-            row1_sum -= grid[0][i];  // Move the top player forward, subtract current value.
-            min_result = min(min_result, max(row1_sum, row2_sum));  // Update the minimum of the max scores.
-            row2_sum += grid[1][i];  // Move the bottom player forward, add current value.
+        int n=grid[0].size();
+        long long remrow1sum=0;
+        for(int i=0;i<n;i++){
+            remrow1sum+=grid[0][i];
         }
-
-        return min_result;  // Return the minimum maximum score.
+        long long row2sum=0;
+        long long mini=LLONG_MAX;
+        for(int i=0;i<n;i++){
+            remrow1sum-=grid[0][i];
+            long long maxi=max(remrow1sum,row2sum);
+            mini=min(mini,maxi);
+            row2sum+=grid[1][i];
+        }
+        return mini;
     }
 };
