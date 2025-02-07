e9268b045122f31b2b69fe609c9fc892ae51d0e2
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2025-02-07 17:55:48 +0530

    Time: 0 ms (100%), Space: 10.8 MB (93.42%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,58 +11,17 @@
  */
 class Solution {
 public:
-/*
-    void solve(TreeNode*root,vector<int>&ans){
-        if(root==NULL) return;
-        ans.push_back(root->val);
-        solve(root->left,ans);
-        solve(root->right,ans);
-    }
     
+    void getans(TreeNode* root,vector<int>&res){
+        if(root==NULL) return;
+        res.push_back(root->val);
+        getans(root->left,res);
+        getans(root->right,res);
+    }
+   
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        solve(root,ans);
-        return ans;
-    }*/
-    /*vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        if(!root) return ans;
-        stack<TreeNode*>st;
-        st.push(root);
-        while(!st.empty()){
-            TreeNode* node=st.top();
-            ans.push_back(node->val);
-            st.pop();
-            if(node->right) st.push(node->right);
-            if(node->left) st.push(node->left);
-        }
-        return ans;
-    }*/
-        //Morris Traversal
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>preorder;
-        TreeNode* curr=root;
-        while(curr){
-            if(!curr->left){
-                preorder.push_back(curr->val);
-                curr=curr->right;
-            }
-            else{
-                TreeNode* temp=curr->left;
-                while(temp->right and temp->right!=curr){
-                    temp=temp->right;
-                }
-                if(!temp->right){
-                    temp->right=curr;
-                    preorder.push_back(curr->val);
-                    curr=curr->left;
-                }
-                else{
-                    temp->right=NULL;
-                    curr=curr->right;
-                }
-            }
-        }
-        return preorder;
+       vector<int>res;
+       getans(root,res);
+       return res;
     }
 };
