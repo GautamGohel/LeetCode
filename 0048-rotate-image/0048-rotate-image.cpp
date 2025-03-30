aa20b244153962cd1f58fc6b81cfd74854150262
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2025-03-30 19:42:32 +0530

    Time: 0 ms (100%), Space: 10.3 MB (34.79%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,9 +3,9 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                swap(matrix[i][j],matrix[j][i]);
-            }
+           for(int j=i+1;j<n;j++){
+             swap(matrix[i][j],matrix[j][i]);
+           }
         }
         for(int i=0;i<n;i++){
             reverse(matrix[i].begin(),matrix[i].end());
