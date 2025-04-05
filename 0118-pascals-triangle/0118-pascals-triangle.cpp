e698a0c284fc2151a66bae65d6abbe95bfc289c4
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2025-04-05 12:09:35 +0530

    Time: 2 ms (15.82%), Space: 9.7 MB (34.22%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-
-   long long calculatencr(int n,int r){
-       long long res = 1;
-
-    for (int i = 0; i < r; i++) {
-        res = res * (n - i);
-        res = res / (i + 1);
-    }
-    return res;
-   }
-
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>finans;
         for(int i=1;i<=numRows;i++){
-            vector<int>ans;
-            for(int j=1;j<=i;j++){
-                long long res=calculatencr(i-1,j-1);
-                ans.push_back(res);
-            }
-            finans.push_back(ans);
+            vector<int>res;
+           int ans=1;
+            res.push_back(1);
+           for(int j=1;j<i;j++){
+              ans=ans*(i-j);
+              ans=ans/j;
+              res.push_back(ans);
+           }
+           finans.push_back(res);
         }
         return finans;
     }
