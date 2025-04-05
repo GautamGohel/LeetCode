4966070234ebb903a87cf492513c9cba555ce110
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2025-04-05 13:19:26 +0530

    Time: 47 ms (73.19%), Space: 28.9 MB (87.56%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,49 +1,32 @@
 class Solution {
 public:
-    // vector<vector<int>> threeSum(vector<int>& nums) {
-    //     int n=nums.size();
-    //     set<vector<int>>st;
-    //     for(int i=0;i<n;i++){
-    //         set<int>hashset;
-    //         for(int j=i+1;j<n;j++){
-    //             int third=-(nums[i]+nums[j]);
-    //             if(hashset.find(third)!=hashset.end()){
-    //                 vector<int>ans={nums[i],nums[j],third};
-    //                 sort(ans.begin(),ans.end());
-    //                 st.insert(ans);
-    //             }
-    //             hashset.insert(nums[j]);
-    //         }
-    //     }
-    //     vector<vector<int>>ans(st.begin(),st.end());
-    //     return ans;
-    // }
-
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>finans;
-        for(int i=0;i<n;i++){
-            if(i>0 and nums[i]==nums[i-1]) continue;
-                int j=i+1;
-                int k=n-1;
-                while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum<0){
-                    j++;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        int n = nums.size();
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
                 }
-                else if(sum>0){
-                    k--;
-                }
-                else{
-                    vector<int>temp={nums[i],nums[j],nums[k]};
-                    finans.push_back(temp);
-                    j++;
-                    k--;
-                    while(j<k and nums[j]==nums[j-1]) j++;
-                    while(j<k and nums[k]==nums[k+1]) k--;
-                }}
+            }
         }
-        return finans;
+        return ans;
     }
 };
