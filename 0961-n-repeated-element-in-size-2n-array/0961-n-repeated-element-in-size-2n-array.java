fa0ac65f3a4f57907732652ce4ad4025a36e4d4a
GautamGohel <103598228+GautamGohel@users.noreply.github.com>
2026-01-02 23:05:54 +0530

    Time: 2 ms (54.01%), Space: 47.5 MB (50.87%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        // HashMap<int,int>mp=new HashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int cnt=0;
+           for(int j=0;j<n;j++){
+             if(nums[i]==nums[j]){
+                cnt++;
+             }
+           }
+           if(cnt==n/2){
+             return nums[i];
+           }
+        }
+        return -1;
+    }
+}
